fd03e6a6ae2ab6e48d43023c515fe709a2763e7b
ilham <e.ilhamfajri@gmail.com>
2019-07-25 16:12:28 +0700

    home edit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # assosiasi
 vsga
+
+login
+
+admin (1,123456)
+ketua (1234,1234)
+sekretaris (2704,2704 | 3001,3001)
+anggota (5555,5555 | 8888,8888 | 9999,9999)
+
--- index.php
+++ index.php
@@ -14,6 +14,7 @@
   <link href="style/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
+  <link rel="stylesheet" href="style/style.css">
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
   <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
@@ -47,7 +48,7 @@
 <body>
 
   <!-- Navigation -->
-  <nav class="navbar navbar-light navbar-inverse bg-light static-top">
+  <nav class="navbar navbar-light navbar-inverse bg-light fixed-top shadow">
     <div class="container-fluid">
       <a class="navbar-brand" href="index.php"><img src="img/logo1.png" height="35px"></a>
       <form class="navbar-form" action="cek_login.php" method="POST">
@@ -68,16 +69,16 @@
     <div class="container">
       <div class="row">
         <div class="col-xl-9 mx-auto">
-          <h1 class="mb-5">Buatlah halaman webmu sendiri bersama kami !</h1>
+          <h1 class="mb-5">Selamat datang di Website Asosiasi Web Programing Indonesia</h1>
         </div>
         <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
           <form method="POST" action="register.php">
             <div class="form-row">
               <div class="col-12 col-md-9  mb-2 mb-md-0">
-                <input type="email" name="email" class="form-control form-control-lg" placeholder="Enter your email...">
+                <input type="email" name="email" class="form-control form-control-lg" placeholder="Masukkan email kamu...">
               </div>
               <div class="col-12 col-md-3">
-                <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
+                <button type="submit" class="btn btn-block btn-lg btn-primary">Daftar!</button>
               </div>
             </div>
           </form>
@@ -87,144 +88,68 @@
   </header>
 
   <!-- Icons Grid -->
-  <section class="features-icons bg-light text-center">
+  <section class="features-icons bg-light text-center m-4">
     <div class="container">
       <div class="row">
-        <div class="col-lg-4">
+        <div class="col-lg-4 border ">
           <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
             <div class="features-icons-icon d-flex">
-              <i class="icon-screen-desktop m-auto text-primary"></i>
+              <i class="icon-home m-auto text-success"></i>
             </div>
-            <h3>Native Programming</h3>
-            <p class="lead mb-0">Belajar menjadi programmer bersama kami</p>
+            <h3>Alamat</h3>
+            <p class="lead mb-0">Batam Centre, Jl. Ahmad Yani, Tlk. Tering, Kec. Batam Kota, Kota Batam, Kepulauan Riau 29461 </p>
+
           </div>
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 border border-right-0 border-left-0">
           <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
             <div class="features-icons-icon d-flex">
-              <i class="icon-layers m-auto text-primary"></i>
+              <i class="icon-phone m-auto text-success"></i>
             </div>
-            <h3>Easy Frameworking</h3>
-            <p class="lead mb-0">Terdapat aneka macam framework atau template programming yang dapat kamu gunakan</p>
+            <h3>Kontak</h3>
+            <p class="lead mb-0">
+              Phone : +62-778-469856
+              <br>
+              Fax : +62-778-463620
+              <br>
+              Email : info@polibatam.ac.id </p>
           </div>
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 border">
           <div class="features-icons-item mx-auto mb-0 mb-lg-3">
             <div class="features-icons-icon d-flex">
-              <i class="icon-check m-auto text-primary"></i>
+              <i class="icon-paper-plane m-auto text-success"></i>
             </div>
-            <h3>Easy to Use</h3>
-            <p class="lead mb-0">Mudah dan cepat dipahami</p>
+            <h3>Misi</h3>
+            <p class="lead mb-0">Belajar Programming Menjadi lebih mudah</p>
           </div>
         </div>
       </div>
     </div>
   </section>
+  
 
-  <!-- Image Showcases -->
-  <section class="showcase">
-    <div class="container-fluid p-0">
-      <div class="row no-gutters">
+ <!--Google map-->
 
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-1.jpg');"></div>
-        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-          <h2>Fully Responsive Design</h2>
-          <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
-        </div>
-      </div>
-      <div class="row no-gutters">
-        <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
-        <div class="col-lg-6 my-auto showcase-text">
-          <h2>Updated For Bootstrap 4</h2>
-          <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!</p>
-        </div>
-      </div>
-      <div class="row no-gutters">
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-3.jpg');"></div>
-        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-          <h2>Easy to Use &amp; Customize</h2>
-          <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- Testimonials -->
-  <section class="testimonials text-center bg-light">
+ <div class="overlay"></div>
     <div class="container">
-      <h2 class="mb-5">What people are saying...</h2>
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt="">
-            <h5>Margaret E.</h5>
-            <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt="">
-            <h5>Fred S.</h5>
-            <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="">
-            <h5>Sarah W.</h5>
-            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
-          </div>
+      <div class="row justify-content-center">
+        <div class="col-lg-10 mb-3">
+        <div id="map-container-google-4" class="z-depth-1-half map-container-4">
+      <iframe class="shadow" src="https://maps.google.com/maps?q=politeknik batam&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+        style="border:solid 3px #F8F9FA" allowfullscreen></iframe>
+    </div>
         </div>
       </div>
     </div>
-  </section>
 
-  <!-- Call to Action -->
-  <section class="call-to-action text-white text-center">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-9 mx-auto">
-          <h2 class="mb-4">Ready to get started? Sign up now!</h2>
-        </div>
-        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-          <form action="register.php" method="POST">
-            <div class="form-row">
-              <div class="col-12 col-md-9 mb-2 mb-md-0">
-                <input type="email" name="email" class="form-control form-control-lg" placeholder="Enter your email...">
-              </div>
-              <div class="col-12 col-md-3">
-                <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
+
 
   <!-- Footer -->
   <footer class="footer bg-dark" style="height:120px; padding-top: 15px; margin-top: 5px">
     <div class="container">
       <div class="row">
         <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
-          <ul class="list-inline mb-2">
-            <li class="list-inline-item">
-              <a href="#">About</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Contact</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Terms of Use</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Privacy Policy</a>
-            </li>
-          </ul>
           <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2019. All Rights Reserved.</p>
         </div>
         <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
